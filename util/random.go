0e7b97c570174bb65783c566ee6454419eb5e72c
maxgoover <maxgoover@gmail.com>
2022-02-23 17:30:26 +0300

    fix generator random values

--- util/random.go
+++ util/random.go
@@ -14,15 +14,15 @@ func init() {
 }
 
 // GenerateRandomInt generates a random integer between min and max
-func GenerateRandomInt(min int64, max int64) int64 {
-	return min + rand.Int63n(max-min+1)
+func GenerateRandomInt(min int32, max int32) int32 {
+	return min + rand.Int31n(max-min+1)
 }
 
-// GenerateRandomString generates a random string of length lengthString
-func GenerateRandomString(stringLength int) string {
+// GenerateRandomString generates a random string of length
+func GenerateRandomString(length int) string {
 	var stringsBuilder strings.Builder
 
-	for i := 0; i < stringLength; i++ {
+	for i := 0; i < length; i++ {
 		randomChar := generateRandomChar()
 		stringsBuilder.WriteByte(randomChar)
 	}
