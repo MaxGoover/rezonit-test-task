77a6029fc91f2d131d8612bec41cd9235c726b65
maxgoover <maxgoover@gmail.com>
2022-02-23 15:10:53 +0300

    generate sqlc golang-code

---
+++ db/query/user.sql
@@ -0,0 +1,37 @@
+-- name: CreateUser :one
+INSERT INTO users (first_name,
+                   last_name,
+                   age)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: GetUser :one
+SELECT *
+FROM users
+WHERE id = $1
+LIMIT 1;
+
+-- name: GetUserForUpdate :one
+SELECT *
+FROM users
+WHERE id = $1
+LIMIT 1 FOR NO KEY UPDATE;
+
+-- name: ListUsers :many
+SELECT *
+FROM users
+ORDER BY id
+LIMIT $1 OFFSET $2;
+
+-- name: UpdateUser :one
+UPDATE users
+SET first_name = $2,
+    last_name = $3,
+    age = $4
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE
+FROM users
+WHERE id = $1;
