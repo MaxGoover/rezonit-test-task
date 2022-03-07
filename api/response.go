2e92906640c594d43e2fd7c8eda0e367114769c9
maxgoover <maxgoover@gmail.com>
2022-03-07 19:43:05 +0300

    fix response

--- api/response.go
+++ api/response.go
@@ -6,32 +6,32 @@ import (
 	"net/http"
 )
 
-type responseJSON struct {
-	result string
-	data   interface{}
+type responseDTO struct {
+	Result string      `json:"result"`
+	Data   interface{} `json:"data"`
 }
 
 func responseError(w http.ResponseWriter, err error, httpStatus int) {
-	m := responseJSON{
-		result: "error",
-		data:   err.Error(),
+	res := &responseDTO{
+		Result: "error",
+		Data:   err.Error(),
 	}
 
-	res, _ := json.Marshal(m)
+	resJSON, _ := json.Marshal(res)
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(httpStatus)
-	fmt.Fprintln(w, string(res))
+	fmt.Fprintln(w, string(resJSON))
 }
 
 func responseOk(w http.ResponseWriter, responseData interface{}) {
-	m := responseJSON{
-		result: "ok",
-		data:   responseData,
+	res := &responseDTO{
+		Result: "ok",
+		Data:   responseData,
 	}
 
-	res, _ := json.Marshal(m)
+	resJSON, _ := json.Marshal(res)
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintln(w, string(res))
+	fmt.Fprintln(w, string(resJSON))
 }
