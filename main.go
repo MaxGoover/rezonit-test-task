6e79cf18b233ae54c1873b4518a753adb84f3721
maxgoover <maxgoover@gmail.com>
2022-03-08 20:51:19 +0300

    delete comments main.go

--- main.go
+++ main.go
@@ -13,23 +13,15 @@ import (
 )
 
 func main() {
-	// Загружаем конфигурацию из конфига приложения
 	config, err := util.LoadConfig(".")
-	// Итого, в config содержится экземпляр структуры Config, заполненный данными из переменной окружения
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
 
-	// Создаем контекст для работы контексто-зависимых частей системы
 	ctx, cancel := context.WithCancel(context.Background())
-
-	// Создаем канал для сигналов ОС
 	c := make(chan os.Signal, 1)
-
-	// В случае поступления сигнала завершения - уведомляем наш канал, бережно закрываем наше приложение
 	signal.Notify(c, os.Interrupt)
 
-	// Создаем соединение с БД и сохраним его для закрытия при остановке приложения
 	conn, err := sql.Open(config.DBDriver, config.DBSource())
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -39,17 +31,12 @@ func main() {
 	log.Println("Starting server...")
 	server := api.NewServer(config, ctx, storage)
 
-	// Горутина для ловли сообщений системы
 	go func() {
-		// Если в операционную систему пришел какой-то сигнал
 		oscall := <-c
 		log.Printf("system call:%+v", oscall)
-		// Останавливаем сервер
 		server.Shutdown()
-		// Отменяем контекст
 		cancel()
 	}()
 
-	// Запускаем сервер
-	server.Start(config.ServerAddress)
+	server.Start()
 }
