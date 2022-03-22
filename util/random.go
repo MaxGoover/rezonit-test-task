d9d46ce612d074b76bf087c9c3d14924f3b3bf90
maxgoover <maxgoover@gmail.com>
2022-03-22 23:27:37 +0300

    refactoring

--- util/random.go
+++ util/random.go
@@ -15,12 +15,10 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-// GenerateRandomInt generates a random integer between min and max
 func GenerateRandomInt(min int32, max int32) int32 {
 	return min + rand.Int31n(max-min+1)
 }
 
-// GenerateRandomString generates a random string of length
 func GenerateRandomString(length int) string {
 	var stringsBuilder strings.Builder
 
